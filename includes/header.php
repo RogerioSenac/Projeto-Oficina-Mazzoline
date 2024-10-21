627d32678d0dfb27d024f123059d7944b9241e53
rogeriosenac <roge.pupo.toledo@gmail.com>
2024-10-21 15:28:51 -0300

    refacture geral

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=na<nav, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Sobre nós</a>
+        <a href="#">Serviços</a>
+        <a href="#">Galeria</a>
+        <a href="#">Contato</a>
+    </nav>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include("./includes/header.php")
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,5 +12,7 @@
 </head>
 <body>
     
+
+
 </body>
 </html>
